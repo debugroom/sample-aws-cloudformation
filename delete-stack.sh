3747668242d64e1eaad0daf044a3c685b4ee3aea
Kohei Kawabata <kohei.kawabata@mac.com>
2019-10-17 16:43:21 +0900

    Add rds, dynamodb, elasticache templates.

--- delete-stack.sh
+++ delete-stack.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+stack_name="sample-cloudformation-elasticache-1"
+#stack_name="sample-cloudformation-dynamodb-1"
+#stack_name="sample-cloudformation-rds-1"
 #stack_name="sample-cloudformation-ecs-service-1"
 #stack_name="sample-cloudformation-ecs-task-1"
 #stack_name="sample-cloudformation-ecs-cluster-1"
 #stack_name="sample-cloudformation-ngw-1"
 #stack_name="sample-cloudformation-alb-1"
 #stack_name="sample-cloudformation-sg-1"
-stack_name="sample-cloudformation-vpc-1"
+#stack_name="sample-cloudformation-vpc-1"
 
 aws cloudformation delete-stack --stack-name ${stack_name}
