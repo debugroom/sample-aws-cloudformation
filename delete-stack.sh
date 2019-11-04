9a11578a74e6296182331b3699b7240ad41a0513
Kohei Kawabata <kohei.kawabata@mac.com>
2019-11-04 13:15:15 +0900

    Debug Git cloune error.

--- delete-stack.sh
+++ delete-stack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#stack_name="sample-cloudformation-codebuild-1"
+stack_name="sample-cloudformation-codebuild-1"
 #stack_name="sample-cloudformation-dynamodb-dev-1"
 #stack_name="sample-cloudformation-sqs-1"
 #stack_name="sample-cloudformation-s3-1"
@@ -13,6 +13,6 @@
 #stack_name="sample-cloudformation-ngw-1"
 #stack_name="sample-cloudformation-alb-1"
 #stack_name="sample-cloudformation-sg-1"
-stack_name="sample-cloudformation-vpc-1"
+#stack_name="sample-cloudformation-vpc-1"
 
 aws cloudformation delete-stack --stack-name ${stack_name}
